3802b28937e649195ea7baa85bc6403fb868ee4d
Nasa <nasa.freaks@gmail.com>
2013-06-24 09:48:23 +0700

    Implemented Ribbit Submission and Public Ribbits Views

--- ribbit/urls.py
+++ ribbit/urls.py
@@ -18,4 +18,6 @@ urlpatterns = patterns('',
     url(r'^login$', 'ribbit_app.views.login_view'),
     url(r'^logout$', 'ribbit_app.views.logout_view'),
     url(r'^signup$', 'ribbit_app.views.signup_view'),
+    url(r'^ribbits$', 'ribbit_app.views.public'),
+    url(r'^submit$', 'ribbit_app.views.submit'),
 )
--- ribbit_app/models.py
+++ ribbit_app/models.py
@@ -16,4 +16,4 @@ class UserProfile(models.Model):
 	def gravatar_url(self):
 		return "http://www.gravatar.com/avatar/%s?s=50" % hashlib.md5(self.user.email).hexdigest()
 
-User.profile = property(lambda u: UserProfile.objects.get or create(user=u)[0])
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
